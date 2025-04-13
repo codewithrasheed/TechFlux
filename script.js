34afa8a0ff71c6dd095b16d95404c8d8f5ab6719
Codewithrasheed <rasheedahmed4845@gmail.com>
2025-04-14 00:02:20 +0500

    updated-blog-website

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-var toggleOpen = document.getElementById('toggleOpen');
-var toggleClose = document.getElementById('toggleClose');
-var collapseMenu = document.getElementById('collapseMenu');
+let loader = document.getElementById("preloader");
+
+window.addEventListener(
+  "load",
+  function (load) {
+    window.removeEventListener("load", load, false);
+    setTimeout(function () {
+      loader.style.display = "none";
+    }, 800);
+},
+  false
+);
+
+
+let toggleOpen = document.getElementById('toggleOpen');
+let toggleClose = document.getElementById('toggleClose');
+let collapseMenu = document.getElementById('collapseMenu');
 
 function handleClick() {
   if (collapseMenu.style.display === 'block') {
@@ -12,3 +26,19 @@ function handleClick() {
 
 toggleOpen.addEventListener('click', handleClick);
 toggleClose.addEventListener('click', handleClick);
+
+document.addEventListener('DOMContentLoaded', () => {
+  let goToTop = document.getElementById('gototop');
+  goToTop.style.display = 'none';
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 100) {
+      goToTop.style.display = 'flex';
+    } else {
+      goToTop.style.display = 'none';
+    }
+  });
+
+  goToTop.addEventListener('click', function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+});
